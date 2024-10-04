c9dc4deaa7ce62b30cb7815e84e12f975ad8a9a8
Noushadozi <noushadozi333@gmail.com>
2024-10-04 19:36:30 +0600

    structure

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { headerLogo } from "../assets/images"
+import { hamburger } from "../assets/icons"
+import { navLinks } from "../constants"
+
+const Nav = () => {
+
+    return (
+        <header className="padding-x py-8 absolute z-10 w-full">
+          
+        </header>
+    )
+}
+
+export default Nav
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+import { arrowRight } from "../assets/icons"
+import Button from "../components/Button"
+import { statistics } from "../constants"
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container"
+    >
+     
+    </section>
+  )
+}
+
+export default Hero
